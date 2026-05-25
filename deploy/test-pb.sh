63950299399665ccce2f67ccef2b28be02290edc
Elhadj Keita <keita.elhadj@gmail.com>
2026-05-25 01:10:32 +0000

    tmp: test endpoint collections/_superusers

--- deploy/test-pb.sh
+++ deploy/test-pb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Test auth PocketBase
+echo "==> Test v0.22+ endpoint"
 curl -s \
-  -X POST http://127.0.0.1:8090/api/superusers/auth-with-password \
+  -X POST http://127.0.0.1:8090/api/collections/_superusers/auth-with-password \
   -H "Content-Type: application/json" \
   -d '{"identity":"keita.elhadj@gmail.com","password":"1985keita"}'
 echo ""

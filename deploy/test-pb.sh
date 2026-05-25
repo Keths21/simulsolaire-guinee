b0addc9fe65d976a53e3534b977acfb794901ef3
Elhadj Keita <keita.elhadj@gmail.com>
2026-05-25 01:37:10 +0000

    chore: suppression script temporaire test-pb.sh

--- deploy/test-pb.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "==> Test v0.22+ endpoint"
-curl -s \
-  -X POST http://127.0.0.1:8090/api/collections/_superusers/auth-with-password \
-  -H "Content-Type: application/json" \
-  -d '{"identity":"keita.elhadj@gmail.com","password":"1985keita"}'
-echo ""
